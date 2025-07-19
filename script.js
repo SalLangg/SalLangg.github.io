d76010882591c6ed66f09dd9ef3a9fcca836a1b0
DavidM <medvedev03dv@gmail.com>
2025-07-19 15:35:34 +0500

    dd

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-const tabs = document.querySelectorAll('.nav-tabs a');
-const sections = document.querySelectorAll('.tab');
+'use strict';
 
-tabs.forEach(tab => {
-  tab.addEventListener('click', e => {
-    e.preventDefault();
+// element toggle function
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
-    // Убираем активность у всех
-    tabs.forEach(t => t.classList.remove('active'));
-    sections.forEach(s => s.classList.remove('active-tab'));
+// sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-    // Активируем нужную вкладку
-    tab.classList.add('active');
-    const target = document.getElementById(tab.dataset.tab);
-    if (target) target.classList.add('active-tab');
+// sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+
+// page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// add event to all nav link
+for (let i = 0; i < navigationLinks.length; i++) {
+  navigationLinks[i].addEventListener("click", function () {
+
+    for (let i = 0; i < pages.length; i++) {
+      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+        pages[i].classList.add("active");
+        navigationLinks[i].classList.add("active");
+        window.scrollTo(0, 0);
+      } else {
+        pages[i].classList.remove("active");
+        navigationLinks[i].classList.remove("active");
+      }
+    }
+
   });
-});
+}
